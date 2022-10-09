4f043086ac619f1d656e00fc4c043c82d339a838
kevinLBermudez <kevin.bermudez6084@unaula.edu.co>
2022-10-09 15:22:25 -0500

    Se hallaron probabilidades y ecuaciones

--- modules/games/__init__.py
+++ modules/games/__init__.py
@@ -1,5 +1,6 @@
 from . import functios_games as games
 import numpy as np
+from sympy import *
 
 def get_teory(data):
 
@@ -7,11 +8,13 @@ def get_teory(data):
     maxiumByColumn = games.maxColumn(data)
     chairPoint = games.chairPoint(miniumByRow,maxiumByColumn)
     matrixProbability = games.matrixProbability(data)
+    equations = games.equations(matrixProbability)
 
     return {
 
-        "minimo por fila" : miniumByRow.tolist(),
-        "maximo por columna" : maxiumByColumn.tolist(),
-        "punto de silla" : chairPoint,
-        "matrix de probabilidades" : matrixProbability.tolist()
+        "minimoPorFila" : miniumByRow.tolist(),
+        "maximoPorColumna" : maxiumByColumn.tolist(),
+        "puntoDeSilla" : chairPoint,
+        "matrixProbabilidades" : matrixProbability.tolist(),
+        "ecuaciones": equations
     }
--- modules/games/functios_games.py
+++ modules/games/functios_games.py
@@ -1,4 +1,7 @@
 import numpy as np
+import sympy as sym
+from sympy import symbols, Eq, solve, Symbol
+from sympy import *
 
 def minRow(matrix):
     miniumByRow = np.amin(matrix, axis=1)
@@ -29,10 +32,11 @@ def matrixProbability(matrix):
     flagCicle = False
     contador = 0 
     x = 4
+    
+    print(matrix)
 
     while flagCicle == False:
 
-        print(flagCicle);
 
         dimensions = matrix.shape
         rows = np.arange(dimensions[0])
@@ -47,7 +51,8 @@ def matrixProbability(matrix):
                             all_are_true = all(x == True for x in c)
                             if all_are_true:
                                 matrix = np.delete(matrix, (i), axis=0)
-                                print("se elimino la fila: ", i)
+                                print("-----------------")
+                                print("se elimino la fila: ", i+1)
                                 print(matrix)
                                 flagColumn = False
                                 flagRow = True
@@ -70,7 +75,8 @@ def matrixProbability(matrix):
                             all_are_true = all(x == True for x in c)
                             if all_are_true:
                                 matrix = np.delete(matrix, (i), axis=1)
-                                print("se elimino la columna: ", i)
+                                print("-----------------")
+                                print("se elimino la columna: ", i+1)
                                 print(matrix)
                                 flagRow = False
                                 flagColumn = True
@@ -88,4 +94,28 @@ def matrixProbability(matrix):
     return matrix
 
 
+def equations(matrix):
+    
+    p = Symbol('p')
+
+    eq1 = matrix[0, 0]*p + matrix[1, 0] * (1-p)
+    eq2 = matrix[0, 1]*p + matrix[1, 1] * (1-p)
+    eq3 = matrix[0,0]*p + matrix[0,1] * (1-p)
+    eq4 = matrix[1,0]*p + matrix[1,1] * (1-p)
+
+    p1 = sym.solve(sym.Eq(eq1,eq2), p)
+    p2 = sym.solve(sym.Eq(eq3,eq4), p)
+
+    eq1 = (eq1.subs(p, p1[0]))/100
+    eq2 = (eq2.subs(p, p1[0]))/100
+    eq3 = (eq3.subs(p, p2[0]))/100
+    eq4 = (eq4.subs(p, p2[0]))/100
+
+    
+    return {"ecuacion1": str(eq1),
+            "ecuacion2": str(eq2),
+            "ecuacion3": str(eq3),
+            "ecuacion4": str(eq4),
+            "probabilidad1": str(p1[0]),
+            "probabilidad2": str(p2[0])}
 
