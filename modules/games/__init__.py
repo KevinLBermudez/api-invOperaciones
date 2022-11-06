beac90e6b91b84ed349ddf9c70142329501be38a
AlexRl-12 <78156140+AlexRL-12@users.noreply.github.com>
2022-11-06 13:31:49 -0500

    cambios en el punto silla

--- modules/games/__init__.py
+++ modules/games/__init__.py
@@ -4,17 +4,27 @@ from sympy import *
 
 def get_teory(data):
 
+    matrixProbability = None
+    equations = None
     miniumByRow = games.minRow(data)
     maxiumByColumn = games.maxColumn(data)
     chairPoint = games.chairPoint(miniumByRow,maxiumByColumn)
-    matrixProbability = games.matrixProbability(data)
-    equations = games.equations(matrixProbability)
+    print(len(chairPoint))
+    if len (chairPoint) < 1 : 
+        
+        matrixProbability = games.matrixProbability(data)
+        equations = games.equations(matrixProbability)
 
     return {
 
         "minimoPorFila" : miniumByRow.tolist(),
         "maximoPorColumna" : maxiumByColumn.tolist(),
         "puntoDeSilla" : chairPoint,
-        "matrixProbabilidades" : matrixProbability.tolist(),
-        "ecuaciones": equations
+        # "matrixProbabilidades" : matrixProbability.tolist(),
+        "matrixProbabilidades": None if matrixProbability is None else matrixProbability.toList(),
+        #"ecuaciones": equations
+        "ecuaciones": None if equations is None else equations
+        
     }
+
+
